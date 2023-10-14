571f4446847b2f5238b129cd65799e3607e831fc
Nathan Pham <87776690+nghiapham1026@users.noreply.github.com>
2023-10-13 22:10:51 -0700

    Sample Website Displaying Fixtures (#1)
    * Displayed sample data from main server
    
    * Fixed fixtures.map error
    
    * View fixtures by dates

--- sportscore/src/App.js
+++ sportscore/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [fixtures, setFixtures] = useState([]);
+    const [error, setError] = useState(null);
+    const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]); // Default to today
+  
+    useEffect(() => {
+      const fetchFixtures = async () => {
+        try {
+          const response = await axios.get('https://sportscore-a1cf52e3ff48.herokuapp.com/fixtures/getFixtures', {
+            params: { date: selectedDate }
+          });
+          setFixtures(response.data);
+        } catch (err) {
+          setError(err.message);
+        }
+      };
+      fetchFixtures();
+    }, [selectedDate]); // Re-run effect when selectedDate changes
+  
+    const handleDateChange = (event) => {
+      setSelectedDate(event.target.value);
+    };
+
+    const minDate = new Date(Date.now() - 14 * 24 * 60 * 60 * 1000).toISOString().split('T')[0];
+    const maxDate = new Date(Date.now() + 14 * 24 * 60 * 60 * 1000).toISOString().split('T')[0];
+  
+    if (error) {
+      return <p>Error: {error}</p>;
+    }
+  
+    return (
+      <div>
+        <h1>Fixtures</h1>
+        <label>
+          Select date: 
+          <input 
+            type="date" 
+            value={selectedDate} 
+            onChange={handleDateChange} 
+            min={minDate}
+            max={maxDate}
+          />
+        </label>
+        <ul>
+          {fixtures.map((fixture, index) => (
+            <li key={index}>
+              <h2>{fixture.league.name}</h2>
+              <p>{fixture.teams.home.name} vs {fixture.teams.away.name}</p>
+              <p>Score: {fixture.score.fulltime.home} - {fixture.score.fulltime.away}</p>
+              {/* Add more fixture details here as needed */}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+};
 
 export default App;
