a50739d083db08ddaa2e83767ecfbf99d616ef5f
Nathan Pham <nghiapham102603@gmail.com>
2023-10-13 21:32:33 -0700

    Displayed sample data from main server

--- sportscore/src/App.js
+++ sportscore/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [fixtures, setFixtures] = useState([]);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      const fetchFixtures = async () => {
+        try {
+          const response = await axios.get('https://sportscore-a1cf52e3ff48.herokuapp.com/fixtures/getFixtures', {
+            params: { date: '2023-10-05' }
+          });
+          setFixtures(response.data);
+        } catch (err) {
+          setError(err.message);
+        }
+      };
+      fetchFixtures();
+    }, []);
+  
+    if (error) {
+      return <p>Error: {error}</p>;
+    }
+  
+    return (
+      <div>
+        <h1>Fixtures</h1>
+        <ul>
+          {fixtures.map((fixture, index) => (
+            <li key={index}>
+              <h2>{fixture.league.name}</h2>
+              <p>{fixture.teams.home.name} vs {fixture.teams.away.name}</p>
+              <p>Score: {fixture.score.fulltime.home} - {fixture.score.fulltime.away}</p>
+              {/* Add more fixture details here as needed */}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
 
 export default App;
